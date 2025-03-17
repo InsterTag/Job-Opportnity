f6ccfa619082c923bf08d2fa70785429643b86d7
alexxtp <wilden087@gmail.com>
2025-03-17 14:47:49 -0500

    prueba

--- database/migrations/2025_03_17_193148_create_job_trainings_table.php
+++ database/migrations/2025_03_17_193148_create_job_trainings_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('job_trainings', function (Blueprint $table) {
             $table->id();
+            //
             $table->timestamps();
         });
     }
