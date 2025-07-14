b9b05efd06b1b3e32b345f97016729c2af210e97
victorcruz <velazcocruzv9@gmail.com>
2025-07-14 15:28:29 -0500

    seeders actualizados

--- database/seeders/NotificationsTableSeeder.php
+++ database/seeders/NotificationsTableSeeder.php
@@ -15,27 +15,32 @@ class NotificationsTableSeeder extends Seeder
         $notifications = [
             [
                 'user_id' => 2, // Asegúrate de que el usuario con ID 1 exista
+                'job_offer_id' => 1, // Asegúrate de que la oferta de trabajo con ID 1 exista
                 'message' => 'Tienes una nueva oferta de trabajo disponible.',
                 'read'    => false,
             ],
             [
                 'user_id' => 4,
+                'job_offer_id' => 2, // Asegúrate de que la oferta de trabajo con ID 2 exista
                 'message' => 'Tu solicitud de empleo fue recibida exitosamente.',
                 'read'    => true,
             ]
             ,
             [
                 'user_id' => 6,
+                'job_offer_id' => 3, // Asegúrate de que la oferta de trabajo con ID 3 exista
                 'message' => 'Tu perfil ha sido actualizado correctamente.',
                 'read'    => false,
             ],
             [
                 'user_id' => 8,
+                'job_offer_id' => 4, // Asegúrate de que la oferta de trabajo con ID 4 exista
                 'message' => 'Tienes un nuevo mensaje en tu bandeja de entrada.',
                 'read'    => true,
             ],
             [
                 'user_id' => 1,
+                'job_offer_id' => 1, // Asegúrate de que la oferta de trabajo con ID 5 exista
                 'message' => 'Tu empresa ha sido verificada.',
                 'read'    => false,
             ]
